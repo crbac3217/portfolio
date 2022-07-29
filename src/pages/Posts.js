a6f220ec688d847507354f5447452af660d93643
crbac3217 <crbac3217@gmail.com>
2022-07-29 02:34:22 -0400

    0729

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,11 +1,17 @@
 import React from "react"
 import {HeaderNav} from '../components/HeaderNav.js'
+import PostTitle from "../components/PostTitle.js"
+import PostList from "../components/PostList.js"
+import Footer from "../components/Footer.js"
 
 const Posts = () => {
 return(
     <div>
     <HeaderNav />
     <div className="hideof">
+    <PostTitle />
+    <PostList />
+    <Footer />
     </div>
     </div>
 )
